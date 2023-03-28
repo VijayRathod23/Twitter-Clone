4e831fa8b015f9a0074b5a4ffacff78e27423608
Harsh-Patel <harsh.patel.esparkbiz.23@gmail.com>
2023-03-28 15:31:24 +0530

    23DEv063 changes in app.js

--- app.js
+++ app.js
@@ -63,9 +63,6 @@ app.use('/',follow);
 const forgot=require('./Routes/forgot')
 app.use('/',forgot);
 
-const loader=require('./Routes/loader')
-app.use('/',loader);
-
 //LogOut API
 app.get("/logout", (req, res) => {
     req.session.destroy(session);
