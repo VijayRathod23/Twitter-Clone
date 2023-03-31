50025046d4bdf91e46de0c7b5f0d07226581cbde
om-modi <you@example.com>
2023-03-31 14:16:07 +0530

    23DEV063 connection credentials set into env file

--- Connection/connection.js
+++ Connection/connection.js
@@ -2,9 +2,9 @@ var mysql = require('mysql2');
 
 var con = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "root",
-    database: "twitter_clone"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 
 });
 
